89f44144ffe679bf8da2f4d7f4f7744822ae057e
zujinhao <zu.jinhao@immomo.com>
2024-09-22 15:34:40 +0800

    修改联系方式

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     contentPrice: 80, // 所有内容的统一价格,单位为积分
     customerService: {
-        contact: "QQ: 3574166785 备注（充值）" // 替换为实际的客服联系方式
+        contact: "QQ: 3574166785 备注（充值） 电报：https://t.me/duduhahh " // 替换为实际的客服联系方式
     }
 };
